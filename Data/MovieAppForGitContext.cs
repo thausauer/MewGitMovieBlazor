a696e4d5d0bc3f0266d3c53322693c0c5bfaf722
Trev Hausuaer <hausauertrev@gmail.com>
2024-11-20 13:36:26 -0600

    Movies DB and Links

---
+++ Data/MovieAppForGitContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorWebAppMovies.Models;
+
+namespace MovieAppForGit.Data
+{
+    public class MovieAppForGitContext : DbContext
+    {
+        public MovieAppForGitContext (DbContextOptions<MovieAppForGitContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorWebAppMovies.Models.Movie> Movie { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
-using MovieAppForGit.Components;
+﻿using MovieAppForGit.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MovieAppForGit.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<MovieAppForGitContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MovieAppForGitContext") ?? throw new InvalidOperationException("Connection string 'MovieAppForGitContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -14,6 +23,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
